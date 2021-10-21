f2db746a2b30807196e0bd79d19f4eaee91deb01
christian.abraham <christian.abraham@email.cz>
2021-10-21 14:39:15 +0200

    Mattias style

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         run();
     }
-
+//Mattias je memer
     private static void run() {
         while(isRunning) {
             System.out.println("Výběr:");
